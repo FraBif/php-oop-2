ea2a3d4990b4944b810172e41be769c74673b63f
FraBif <francescobif@hotmail.it>
2023-07-24 16:53:47 +0200

    added another item in page

--- db/init.php
+++ db/init.php
@@ -13,9 +13,11 @@ $dogItems = new Category('Dog Items', 'Items for dogs', '');
 $woolBall = new Toy('Wool ball', 'Ball of  wool for small cats', 12.99, 'https://media.istockphoto.com/id/1263674144/photo/red-ball-with-woolen-threads-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=L7UnV596Hi5HMzG4XjXfIhDC4SyxO_KUsA1Iegetf_0=' , true, $catItems, 13, 'Small', 'wool');
 $biscuits = new Food('Big Dog Biscuits', 'Biscuits for the biggest and proudest dogs!', 33.33, 'https://www.nutrixpiu.it/wp-content/uploads/2018/08/adulti-taglia-grande-nutrix-piu-2kg-crocchette.png
 ', true, $dogItems, 30, 350, 8);
+$leash = new Toy('Leather Leash', 'A classy leash for a classy dog', 30.00, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeQ0XaoDR1478Hz17k1FFqTSo3zvT7RacMzyGAJl_tIynanNiqg5xKS8VcqDMS4RvReIw&usqp=CAU', true, $dogItems, 12, 'Medium', 'leather');
 
 
 $products = [
     $woolBall,
-    $biscuits
+    $biscuits,
+    $leash
 ];
